67d7183e2c181a879e4427c4cbc55ad15102abf5
Diogo Carmo <info@diogo.ch>
2022-11-23 13:25:00 +0100

    generate controller

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,16 @@
+class BookingsController < ApplicationController
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      flash[:alert] = @booking.errors.full_messages.join("\n")
+      render :new, status: :unprocessable_entity # message d'erreur
+    end
+    private
+
+    def booking_params
+      params.require(:booking).permit(:title, :description, :location, :hourly_rate)
+    end
+end
