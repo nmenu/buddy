65dc1bb7ecdb7b1f5a4d9448c56b79271a45e2d8
nicolas menu <nicolas.menu@wild-lights.com>
2022-11-23 11:18:40 +0100

    Afficher les offres, Css kitt cards controler views,...

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: "pages#home"
+  resources :offers, only: [:index]
 
-  resources :pages, only: [:index]
-
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
