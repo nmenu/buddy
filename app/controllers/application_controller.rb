e3f19a7539759ffa1f8ea10bf0a40472c3eb0615
nicolas menu <nicolas.menu@wild-lights.com>
2022-11-24 16:14:04 +0100

    Added working mechanism to edit profile and a conditional second button on home if logged in or not

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,27 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+    # [...]
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+    protected
+
+    def configure_permitted_parameters
+      # For additional fields in app/views/devise/registrations/new.html.erb
+      # devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+
+      # For additional in app/views/devise/registrations/edit.html.erb
+      devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :street, :city, :npa])
+    end
+
 end
+
+
+
+# if @user.save
+#   redirect_to root_path()
+# else
+#   flash[:alert] = @booking.errors.full_messages.join("\n")
+#   render :new, status: :unprocessable_entity # message d'erreur
+# end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-
-
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: "pages#home"
+
   resources :offers, only: [:index, :new, :create, :show]
   resources :bookings
 
