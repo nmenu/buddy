e5f9d774a10299c6fc75b193757beda967ada8bc
nicolas menu <nicolas.menu@wild-lights.com>
2022-11-25 16:16:01 +0100

    n importe

--- Gemfile
+++ Gemfile
@@ -4,6 +4,9 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby "3.1.2"
 
 # Gemfile
+gem "cloudinary"
+
+# Gemfile
 gem "devise"
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
-  has_many :my_offers, foreign_key: 'user_id', class_name: 'Offer'
-  has_many :bookings
-  has_many :offers, through: :bookings
+  has_many :my_offers, foreign_key: 'user_id', class_name: 'Offer', dependent: :destroy
+  has_many :bookings, dependent: :destroy
+  has_many :offers, through: :bookings, dependent: :destroy
+
+  has_one_attached :photo
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
