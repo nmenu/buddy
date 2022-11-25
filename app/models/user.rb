e925ceaae61fd01d461830ebad44e0f711eff1ee
Diogo Carmo <info@diogo.ch>
2022-11-25 16:46:20 +0100

    bookings and offers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :my_offers, foreign_key: 'user_id', class_name: 'Offer'
+  has_many :my_bookings, through: :my_offers, source: :bookings
   has_many :bookings
   has_many :offers, through: :bookings
   # Include default devise modules. Others available are:
